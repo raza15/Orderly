907e9845b327b1f80e1d3779396272e29fffd3e6
Raza Hussain <16100251@lums.edu.pk>
2017-11-12 10:19:17 -0500

    first commit - sunday 10:19 am

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,136 @@
+class SessionsController < ApplicationController
+  before_action :set_session, only: [:show, :edit, :update, :destroy]
+
+  # GET /sessions
+  # GET /sessions.json
+  def index
+    @sessions = Session.all
+  end
+
+  # GET /sessions/1
+  # GET /sessions/1.json
+  def show
+  end
+
+  def creditpayment
+
+  end
+
+  def end_transaction
+
+  end
+
+  def creditpayment_process
+    require 'net/http'
+    require 'uri'
+    require 'json'
+
+    secureNetId = '8011195' # Replace with your own ID
+    secureKey = '6kGnYyjuVs4b' # Replace with your own Key
+    url = 'https://gwapi.demo.securenet.com/api/Payments/Charge'
+    charge = {
+      amount: 8.00,
+      card: {
+        number: '4111111111111111',
+        cvv: '123',
+        expirationDate: '07/2018',
+        address: {
+          company: 'Nov8 Inc',
+          line1: '123 Main St.',
+          city: 'Austin',
+          state: 'TX',
+          zip: '78759'
+        }
+      },
+      extendedInformation: {
+        typeOfGoods: 'PHYSICAL'
+      },
+      developerApplication: {
+        developerId: 12345678,
+        version: '1.2'
+      }
+    }
+
+    uri = URI.parse(url)                       # Parse the URI
+    http = Net::HTTP.new(uri.host, uri.port)   # New HTTP connection
+    http.use_ssl = true                        # Must use SSL!
+    req = Net::HTTP::Post.new(uri.request_uri) # HTTP POST request 
+    req.body = charge.to_json                  # Convert hashmap to string
+    req["Content-Type"] = 'application/json'   # JSON body
+    req.basic_auth secureNetId, secureKey      # HTTP basic auth
+    res = http.request(req)                    # Make the call
+    res_body = JSON.parse(res.body)            # Convert JSON to hashmap
+
+    puts "http response code: #{res.code}"
+    puts "success: #{res_body["success"]}"
+    puts "result: #{res_body["result"]}"
+    puts "message: #{res_body["message"]}"
+    puts "transactionId: #{res_body["transaction"]["transactionId"]}"
+  end
+
+  # GET /sessions/new
+  def new
+    cookies[:orders] = []
+    cookies[:coffee_order] = "coffee"
+    cookies[:coffee_quantity] = 1
+    cookies[:coffee_size] = "medium"
+    cookies[:total_cost] = 0
+    @session = Session.new
+    session[:order_id] = 5
+  end
+
+  # GET /sessions/1/edit
+  def edit
+  end
+
+  # POST /sessions
+  # POST /sessions.json
+  def create
+    @session = Session.new(session_params)
+
+    respond_to do |format|
+      if @session.save
+        format.html { redirect_to @session, notice: 'Session was successfully created.' }
+        format.json { render :show, status: :created, location: @session }
+      else
+        format.html { render :new }
+        format.json { render json: @session.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /sessions/1
+  # PATCH/PUT /sessions/1.json
+  def update
+    respond_to do |format|
+      if @session.update(session_params)
+        format.html { redirect_to @session, notice: 'Session was successfully updated.' }
+        format.json { render :show, status: :ok, location: @session }
+      else
+        format.html { render :edit }
+        format.json { render json: @session.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /sessions/1
+  # DELETE /sessions/1.json
+  def destroy
+    @session.destroy
+    respond_to do |format|
+      format.html { redirect_to sessions_url, notice: 'Session was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_session
+      @session = Session.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def session_params
+      params.require(:session).permit(:order_id)
+    end
+end
---
+++ public/app.js
@@ -0,0 +1,228 @@
+function isChrome() {
+  var isChromium = window.chrome,
+    winNav = window.navigator,
+    vendorName = winNav.vendor,
+    isOpera = winNav.userAgent.indexOf("OPR") > -1,
+    isIEedge = winNav.userAgent.indexOf("Edge") > -1,
+    isIOSChrome = winNav.userAgent.match("CriOS");
+
+  if(isIOSChrome){
+    return true;
+  } else if(isChromium !== null && isChromium !== undefined && vendorName === "Google Inc." && isOpera == false && isIEedge == false) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function gotoListeningState() {
+  const micListening = document.querySelector(".mic .listening");
+  const micReady = document.querySelector(".mic .ready");
+
+  micListening.style.display = "block";
+  micReady.style.display = "none";
+}
+
+function gotoReadyState() {
+  const micListening = document.querySelector(".mic .listening");
+  const micReady = document.querySelector(".mic .ready");
+
+  micListening.style.display = "none";
+  micReady.style.display = "block";
+}
+
+function addBotItem(text) {
+  const appContent = document.querySelector(".app-content");
+  appContent.innerHTML += '<div class="item-container item-container-bot"><div class="item"><p>' + text + '</p></div></div>';
+  appContent.scrollTop = appContent.scrollHeight; // scroll to bottom
+}
+
+function addUserItem(text) {
+  const appContent = document.querySelector(".app-content");
+  appContent.innerHTML += '<div class="item-container item-container-user"><div class="item"><p>' + text + '</p></div></div>';
+  appContent.scrollTop = appContent.scrollHeight; // scroll to bottom
+}
+
+function displayCurrentTime() {
+  const timeContent = document.querySelector(".time-indicator-content");
+  const d = new Date();
+  const s = d.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+  timeContent.innerHTML = s;
+}
+
+function addError(text) {
+  addBotItem(text);
+  const footer = document.querySelector(".app-footer");
+  footer.style.display = "none";
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  // test for relevant API-s
+  // for (let api of ['speechSynthesis', 'webkitSpeechSynthesis', 'speechRecognition', 'webkitSpeechRecognition']) {
+  //   console.log('api ' + api + " and if browser has it: " + (api in window));
+  // }
+
+  displayCurrentTime();
+
+  // check for Chrome
+  if (!isChrome()) {
+    addError("This demo only works in Google Chrome.");
+    return;
+  }
+
+  if (!('speechSynthesis' in window)) {
+    addError("Your browser doesn’t support speech synthesis. This demo won’t work.");
+    return;
+  }
+
+  if (!('webkitSpeechRecognition' in window)) {
+    addError("Your browser cannot record voice. This demo won’t work.");
+    return;
+  }
+
+  // Now we’ve established that the browser is Chrome with proper speech API-s.
+
+  // api.ai client
+  const apiClient = new ApiAi.ApiAiClient({accessToken: 'e7c9e0f5beb44406acd8f52f2602b1fc'});
+
+  // Initial feedback message.
+  addBotItem("Hi! I’m voicebot. Tap the microphone and start talking to me.");
+
+  var recognition = new webkitSpeechRecognition();
+  var recognizedText = null;
+  recognition.continuous = false;
+  recognition.onstart = function() {
+    // alert("here in onstart");
+    // document.getElementById("p1").innerHTML = "Order Started!";
+    recognizedText = null;
+  };
+  recognition.onresult = function(ev) {
+    recognizedText = ev["results"][0][0]["transcript"];
+    addUserItem(recognizedText);
+    ga('send', 'event', 'Message', 'add', 'user');
+
+    let promise = apiClient.textRequest(recognizedText);
+
+    promise
+        .then(handleResponse)
+        .catch(handleError);
+
+    function handleResponse(serverResponse) {
+
+      // Set a timer just in case. so if there was an error speaking or whatever, there will at least be a prompt to continue
+      var timer = window.setTimeout(function() { startListening(); }, 20000);
+
+      const speech = serverResponse["result"]["fulfillment"]["speech"];
+      var msg = new SpeechSynthesisUtterance(speech);
+      addBotItem(speech);
+      console.log("lelz");
+      var orders_array = jQuery.cookie("orders").split('&');
+      // console.log(orders_array);
+      console.log(serverResponse);
+      var all_items = ["donut","coffee"];
+      var all_prices = [1.5,2];
+      var default_price = 3;
+      var tax_rate = 0.10;
+      var action = serverResponse["result"]["action"];
+      var total_cost = parseInt(jQuery.cookie("total_cost"));
+      
+      if( serverResponse["result"]["metadata"]["intentName"]=="order.coffee" ) {
+        if(serverResponse["result"]["fulfillment"]["speech"]=="Ok, what size?") {
+          if(isNaN(serverResponse["result"]["parameters"]["number"])==false) {
+            var num = parseInt(isNaN(serverResponse["result"]["parameters"]["number"]));
+            $.cookie("total_cost", num);
+          }
+        }
+
+
+      } else if(action == "add_item") {
+        var donut_types = serverResponse["result"]["parameters"]["donutType"]
+        var donut_numbers = serverResponse["result"]["parameters"]["number"]
+        if(donut_types.length!=0) {
+          for(i=0; i<donut_types.length; i++) {
+            var donut_type = donut_types[i];
+            var donut_count;
+            if(i<donut_numbers.length) {
+              donut_count = donut_numbers[i];
+              orders_array.push(donut_type);
+              orders_array.push(donut_count);
+              var cost = default_price*donut_count;
+              if(donut_type.includes("donut")) {
+                cost = all_prices[0]*donut_count;
+              }else if(donut_type.includes("coffee")) {
+                cost = all_prices[1]*donut_count;
+              }
+              console.log(total_cost);
+              total_cost = total_cost + cost;
+              document.getElementById("p1").innerHTML += donut_count+" "+donut_type+" : $"+cost+"<br>"
+              document.getElementById("p2").innerHTML = " TOTAL COST: $"+total_cost+"<br>";
+            }
+          }
+          $.cookie("orders", orders_array);
+          $.cookie("total_cost", total_cost);
+        }
+      } else if(action=="confirm_order") {
+        var msg1 = new SpeechSynthesisUtterance("Perfect. Your total will be "+total_cost+" dollars. You are now redirected to the payments page.");
+        window.speechSynthesis.speak(msg1);
+        window.location.href = "/creditpayment";
+      }
+      // if( speech.startsWith("Showing you different types of donuts") ) {
+      //   document.getElementById("img1").src="/images/dunkin-donut.jpg";
+      // } else if( speech.startsWith("Showing you different types of hot drinks") ) {
+      //   document.getElementById("img1").src="/images/hot-drinks.jpg";
+      // } else if( speech.includes("donut confirmed") ) {
+      //   document.getElementById("p1").innerHTML += "1 donut : $2<br>"
+      // }
+      ga('send', 'event', 'Message', 'add', 'bot');
+      msg.addEventListener("end", function(ev) {
+        window.clearTimeout(timer);
+        startListening();
+      });
+      msg.addEventListener("error", function(ev) {
+        window.clearTimeout(timer);
+        startListening();
+      });
+
+      window.speechSynthesis.speak(msg);
+    }
+    function handleError(serverError) {
+      console.log("Error from api.ai server: ", serverError);
+    }
+  };
+
+  recognition.onerror = function(ev) {
+    console.log("Speech recognition error", ev);
+  };
+  recognition.onend = function() {
+    gotoReadyState();
+    // document.getElementById("p1").innerHTML = "Order Ended!";
+  };
+
+  function startListening() {
+    gotoListeningState();
+    recognition.start();
+  }
+
+  const startButton = document.querySelector("#start");
+  startButton.addEventListener("click", function(ev) {
+    ga('send', 'event', 'Button', 'click');
+    startListening();
+    ev.preventDefault();
+  });
+
+  // Esc key handler - cancel listening if pressed
+  // http://stackoverflow.com/questions/3369593/how-to-detect-escape-key-press-with-javascript-or-jquery
+  document.addEventListener("keydown", function(evt) {
+    evt = evt || window.event;
+    var isEscape = false;
+    if ("key" in evt) {
+        isEscape = (evt.key == "Escape" || evt.key == "Esc");
+    } else {
+        isEscape = (evt.keyCode == 27);
+    }
+    if (isEscape) {
+        recognition.abort();
+    }
+  });
+});
